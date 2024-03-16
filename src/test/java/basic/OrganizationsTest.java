d6726c3199d537bd9be42b68249a0e6f95c1bfde
abhijix <abhijix@gmail.com>
2024-03-16 13:15:25 +0530

    update project

---
+++ src/test/java/basic/OrganizationsTest.java
@@ -0,0 +1,15 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationsTest {
+	@Test
+	public void CreateOrganizationsTest() 
+	{
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://localhost:8888");
+	}
+
+}
